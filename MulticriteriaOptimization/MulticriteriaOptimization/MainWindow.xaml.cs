5c2cf6372acde1fd5feea3d85460ddb728264724
bibipkins <bibipkins@gmail.com>
2017-03-13 19:34:20 +0200

    Complete criteria tab

--- MulticriteriaOptimization/MulticriteriaOptimization/MainWindow.xaml.cs
+++ MulticriteriaOptimization/MulticriteriaOptimization/MainWindow.xaml.cs
@@ -24,5 +24,54 @@ namespace MulticriteriaOptimization
         {
             InitializeComponent();
         }
+
+        private void btnAddCriteria_Click(object sender, RoutedEventArgs e)
+        {
+            float weight = 0;
+            
+            try
+            {
+                weight = float.Parse(txtCriteriaWeight.Text.Replace('.', ','));              
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid weight!", "Cant add criteria");
+                return;
+            }
+
+            if (weight >= 0 && weight <= 1)
+            {
+                if(!String.IsNullOrEmpty(txtCriteriaName.Text) && !String.IsNullOrWhiteSpace(txtCriteriaName.Text))
+                {
+                    var data = new CriteriaPrototype { Name = txtCriteriaName.Text, Weight = weight.ToString() };
+                    dataGridCriteria.Items.Add(data);
+                }
+                else
+                {
+                    MessageBox.Show("Invalid name!", "Cant add criteria");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid weight! It mast be in [0,1] range", "Cant add criteria");
+            }
+
+        }
+        private void btnDeleteCriteria_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = dataGridCriteria.SelectedItems;
+            int itemsCount = selectedItems.Count;
+
+            for (int i = 0; i < itemsCount; i++)
+            {
+                dataGridCriteria.Items.Remove(selectedItems[0]);
+            }
+        }
+    }
+
+    public class CriteriaPrototype
+    {
+        public string Name { get; set; }
+        public string Weight { get; set; }
     }
 }
